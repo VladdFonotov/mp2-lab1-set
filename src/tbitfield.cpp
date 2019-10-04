e5a5376cbb5d877481a706a79fe9e1950d2e0ab2
VladdFonotov <55320090+VladdFonotov@users.noreply.github.com>
2019-10-04 12:04:50 +0300

    Update tbitfield.cpp

--- src/tbitfield.cpp
+++ src/tbitfield.cpp
@@ -1,30 +1,48 @@
-// ННГУ, ВМК, Курс "Методы программирования-2", С++, ООП
-//
-// tbitfield.cpp - Copyright (c) Гергель В.П. 07.05.2001
-//   Переработано для Microsoft Visual Studio 2008 Сысоевым А.В. (19.04.2015)
-//
-// Битовое поле
+
 
 #include "tbitfield.h"
 
-TBitField::TBitField(int len)
+TBitField::TBitField(int len=15)
 {
+  BitLen = len;
+	MemLen = (len - 1) / 32 + 1;
+	pMem = new TELEM[MemLen];
+	for (int i = 0; i < MemLen; i++)
+	{
+		pMem[i] = 0;
+	}
 }
 
 TBitField::TBitField(const TBitField &bf) // конструктор копирования
 {
+  BitLen = bf.BitLen;
+	MemLen = bf.MemLen;
+	pMem = new TELEM[MemLen];
+	for (int i = 0; i < MemLen; i++)
+	{
+		pMem[i] = bf.pMem[i];
+	}
 }
 
 TBitField::~TBitField()
 {
+  delete[]pMem;
+
 }
 
 int TBitField::GetMemIndex(const int n) const // индекс Мем для бита n
 {
+  return n / 32;
+}
 }
 
 TELEM TBitField::GetMemMask(const int n) const // битовая маска для бита n
 {
+  int v;
+	v = n % 32;
+	TELEM tmp = 1;
+	tmp = tmp << v;
+	return tmp;
 }
 
 // доступ к битам битового поля
